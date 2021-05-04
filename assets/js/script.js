bda2a3b7fc4661e5da447df0d05df96caae124cc
sdpercy <scottpercy@hotmail.com>
2021-05-04 15:23:53 -0230

    display proper info in task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,25 @@ var createTaskHandler = function() {
 
     event.preventDefault();
 
-    var listItemEl = document.createElement("li");//Create a new task item.
-    listItemEl.className = "task-item";//Style the new task item.
-    listItemEl.textContent = "This is a new task.";//Add the text.
-    tasksToDoEl.appendChild(listItemEl);//Append this element to the task list.
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
   };
 
   formEl.addEventListener("submit", createTaskHandler);
